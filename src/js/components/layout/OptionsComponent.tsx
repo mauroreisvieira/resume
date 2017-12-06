4cc990ce55276f7aca6392dfe4eb1b41b75ed313
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2017-12-06 21:45:56 +0000

    - Fixed form options.

--- src/js/components/layout/OptionsComponent.tsx
+++ src/js/components/layout/OptionsComponent.tsx
@@ -9,27 +9,48 @@ export class OptionsComponent extends React.Component  {
         return (
             <aside className='aside'>
                 <form action='' className='form'>
-                    <div className='form__field'>
-                        <label htmlFor='fieldName' className='form__label'>
-                            Name <span className='text-red'>*</span>
-                        </label>
-                        <input id='fieldName' type='text' name='' placeholder='' className='form__input form__label--large' />
-                    </div>
-                    <div className='form__field'>
-                        <label htmlFor='fieldEmail' className='form__label'>
-                            Email <span className='text-red'>*</span>
-                        </label>
-                        <input id='fieldEmail' type='email' name='' placeholder='' className='form__input form__label--large' />
-                    </div>
-                    <div className='form__field'>
-                        <label htmlFor='fieldMessage' className='form__label'>
-                            Message<span className='text-red'>*</span>
-                        </label>
-                        <textarea id='fieldMessage' className='form__textarea'></textarea>
-                    </div>
-                    <div className='form__actions'>
-                        <button className='button button--info'>Send</button>
-                        <button className='button button--default'>Reset</button>
+                    <div className='row'>
+                        <div className='form__field col-large-12'>
+                            <label htmlFor='postionApplying' className='form__label'>
+                                Position you are applying for
+                            </label>
+                            <input id='postionApplying' type='text' value='Senio Front end Developer' className='form__input form__label--large' />
+                        </div>
+                        <div className='form__field col-large-6'>
+                            <label htmlFor='firstName' className='form__label'>
+                                First Name
+                            </label>
+                            <input id='firstName' type='text' value='Mauro' placeholder='' className='form__input form__label--large' />
+                        </div>
+                         <div className='form__field col-large-6'>
+                            <label htmlFor='lastName' className='form__label'>
+                                Last Name
+                            </label>
+                            <input id='lastName' type='text' value='Reis Vieirs' placeholder='' className='form__input form__label--large' />
+                        </div>
+                        <div className='form__field col-large-6'>
+                            <label htmlFor='phoneNumber' className='form__label'>
+                                Phone Number
+                            </label>
+                            <input id='phoneNumber' type='text' value='917771054' placeholder='' className='form__input form__label--large' />
+                        </div>
+                        <div className='form__field col-large-6'>
+                            <label htmlFor='fieldEmail' className='form__label'>
+                                Email
+                            </label>
+                            <input id='fieldEmail' type='email' value='mauroreisvieira@gmail.com' placeholder='' className='form__input form__label--large' />
+                        </div>
+                        <div className='form__field col-large-12'>
+                            <label htmlFor='fieldMessage' className='form__label'>
+                                Message
+                            </label>
+                            <textarea id='fieldMessage' className='form__textarea' rows='6'>
+                                I'm a Portuguese developer, fascinated by web development, who likes what they do. I love to learn, and share my knowledge, in order to enrich my portfolio. I'm an "Apple Fanboy", as well as a Sublime Text evangelist (I love this text editor for code).
+                            </textarea>
+                        </div>
+                        <div className='form__actions col-large-6'>
+                            <button className='button button--large button--success'>Save Changes</button>
+                        </div>
                     </div>
                 </form>
             </aside>
